dca9e26d7176deee64fc4fa6e3098e3e7898c0a6
jacobtye <jacob.h.tye@gmail.com>
2020-03-25 19:43:14 -0600

    formated code

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost:27017/scriptures', {
-  useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,7 +29,9 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
@@ -43,110 +46,139 @@ const README = new Scripture({
   userName: "all",
   displayName: "all",
   reference: "README",
-  contents:"This is a site I created for my Book of Mormon class where" +
-            " people can write about their favorite scriptures in the " + 
-            "Book of Mormon to share with others. Simply type in a reference, " +
-            "type in your reasons why it is special, and choose your preferences and hit save." +
-            " it will only be publically available if you give permission.",
+  contents: "This is a site I created for my Book of Mormon class where" +
+    " people can write about their favorite scriptures in the " +
+    "Book of Mormon to share with others. Simply type in a reference, " +
+    "type in your reasons why it is special, and choose your preferences and hit save." +
+    " it will only be publically available if you give permission.",
   verse: "",
   publicAllowed: true,
 });
 app.post('/saveScripture', async (req, res) => {
-    console.log("in post");
-    console.log(req.body);
+  console.log("in post");
+  console.log(req.body);
   console.log(req.body.reference);
-  Scripture.findOneAndUpdate({userName: req.params.id, reference: req.body.reference},{ "$set":
-  {contents: req.body.contents, displayName: req.body.displayName, reference: req.body.reference, userName: req.body.userName,
-    publicAllowed: req.body.publicAllowed, verse: req.body.verse
-  } }, {upsert: true}, 
-  function(err,post){
-        if (err) {
-          return res.send(500, {error: err});
-          
-        };
-    console.log(post);
-    return res.send('Succesfully saved.');
-  });
+  Scripture.findOneAndUpdate({
+      userName: req.params.id,
+      reference: req.body.reference
+    }, {
+      "$set": {
+        contents: req.body.contents,
+        displayName: req.body.displayName,
+        reference: req.body.reference,
+        userName: req.body.userName,
+        publicAllowed: req.body.publicAllowed,
+        verse: req.body.verse
+      }
+    }, {
+      upsert: true
+    },
+    function (err, post) {
+      if (err) {
+        return res.send(500, {
+          error: err
+        });
+
+      };
+      console.log(post);
+      return res.send('Succesfully saved.');
+    });
 });
 
 app.post('/saveTemp/:id', (req, res) => {
-    console.log("in post");
-  Scripture.findOneAndUpdate({userName: req.params.id, reference: req.body.reference},{contents: req.body.contents}, {upsert: true}, 
-  function(err,post){
-        if (err) {
-          return res.send(500, {error: err});
-          
-        }
-    console.log(post);
-    return res.send('Succesfully saved.');
-  });
+  console.log("in post");
+  Scripture.findOneAndUpdate({
+      userName: req.params.id,
+      reference: req.body.reference
+    }, {
+      contents: req.body.contents
+    }, {
+      upsert: true
+    },
+    function (err, post) {
+      if (err) {
+        return res.send(500, {
+          error: err
+        });
+
+      }
+      console.log(post);
+      return res.send('Succesfully saved.');
+    });
 });
 app.get('/getREADME', (req, res) => {
-    console.log("in post");
-    console.log(req.body);
+  console.log("in post");
+  console.log(req.body);
   console.log(README);
   res.send(README);
 });
-app.get('/getTemp/:id', async (req,res) =>{
+app.get('/getTemp/:id', async (req, res) => {
   try {
     console.log("GETTING DOCUMENTS" + req.params.id);
-    let temp = await Scripture.find({userName: req.params.id, reference: "recovery"});
-    console.log("TEMP"+ temp);
+    let temp = await Scripture.find({
+      userName: req.params.id,
+      reference: "recovery"
+    });
+    console.log("TEMP" + temp);
     return res.send(temp);
-  }catch (error) {
+  } catch (error) {
     return res.sendStatus(500);
   }
 });
 app.get('/getScriptures/:id', async (req, res) => {
   try {
     console.log("GETTING Scripture" + req.params.id);
-    let scriptures = await Scripture.find({userName: req.params.id});
+    let scriptures = await Scripture.find({
+      userName: req.params.id
+    });
     return res.send(scriptures);
-  }catch (error) {
+  } catch (error) {
     return res.sendStatus(500);
   }
 });
 app.get('/getAllPublic', async (req, res) => {
   try {
     console.log("GETTING Scripture" + req.params.id);
-    let scriptures = await Scripture.find({publicAllowed: true});
+    let scriptures = await Scripture.find({
+      publicAllowed: true
+    });
     return res.send(scriptures);
-  }catch (error) {
+  } catch (error) {
     return res.sendStatus(500);
   }
 });
 app.get('/getChanged', (req, res) => {
-    console.log("in get");
-    console.log(req.body);
-    console.log(this.changed);
-  
+  console.log("in get");
+  console.log(req.body);
+  console.log(this.changed);
+
   res.send(this.changed);
-  if(this.changed){
+  if (this.changed) {
     this.changed = false;
   }
 });
-app.delete('/deleteScripture/:id', async (req,res) => {
+app.delete('/deleteScripture/:id', async (req, res) => {
   console.log("IN DELETE");
   console.log(req.params.id);
-  try{
+  try {
     await Scripture.deleteOne({
       _id: req.params.id
     });
     return res.sendStatus(200);
-  } catch (error){
-    
+  } catch (error) {
+
   }
 });
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
